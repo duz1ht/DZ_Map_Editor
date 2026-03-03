224f80fd44aec4946f28fd3fd363ec94ec27e3af
duz1ht <eduardomacedot@gmail.com>
2026-03-03 01:11:00 -0300

    Updated otheritems.js

--- resources/otheritems.js
+++ resources/otheritems.js
@@ -1,124 +1,123 @@
+begin "Waypoint"
+	type_id 106005
+	graphic waypoint
+end
+
+
+begin "Area Trigger"
+	type_id 106013
+	graphic waypoint
+end
+
+
+
+
 begin "start, Blue Team"
-	type_id 6003
+	type_id 106003
 	graphic Person
 end
 
 
 
 begin "start, Red Team"
-	type_id 6004
+	type_id 106004
 	graphic Person
 end
 
 
-
-begin "Waypoint"
-	type_id 6005
-	graphic waypoint
-end
-
-
-
-begin "Area Trigger"
-	type_id 6013
-	graphic waypoint
-end
-
-
-
 begin "Briefing Map Center"
-	type_id 6038
+	type_id 106038
 	graphic
 end
 
 
 
 begin "start, player"
-	type_id 6001
-	graphic
+	type_id 106001
+	graphic Person
 end  
 
 
 
 begin "start, dmatch"
-	type_id 6002
-	graphic
+	type_id 106002
+	graphic Person
 end  
 
 
 
 begin "start, primary, player"
-	type_id 6094
-	graphic
+	type_id 106094
+	graphic Person
 end  
 
 
 
 begin "start, primary, dmatch"
-	type_id 6095
-	graphic
+	type_id 106095
+	graphic Person
 end  
 
 
 
 begin "start, primary, Blue Team"
-	type_id 6096
-	graphic
+	type_id 106096
+	graphic Person
 end  
 
 
 
 begin "start, primary, Red Team"
-	type_id 6097
-	graphic
+	type_id 106097
+	graphic Person
 end  
 
 
 
 begin "teleport target"
-	type_id 6088
+	type_id 106088
 	graphic
 end  
 
 
 
 begin "user waypoint"
-	type_id 6089
-	graphic
+	type_id 106089
+	graphic waypoint
 end  
 
 
 
 begin "waypoint, mp, alpha"
-	type_id 6026
-	graphic
+	type_id 106026
+	graphic waypoint
 end  
 
 
 
 begin "waypoint, mp, red base"
-	type_id 6027
-	graphic
+	type_id 106027
+	graphic waypoint
 end  
 
 
 
 begin "waypoint, mp, blue base"
-	type_id 6028
-	graphic
+	type_id 106028
+	graphic waypoint
 end  
 
 
 
 begin "KOTH center"
-	type_id 6006
+	type_id 106006
 	graphic
 end  
 
 
 
 begin "Map Centerpoint, helps align commander map grid"
-	type_id 2043
+	type_id 102043
 	graphic
 end 
 
